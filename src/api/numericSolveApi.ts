09c0f06d2ff276b7900a3260ba18cc197875d365
jvictordev1 <josevictorcruzrb@gmail.com>
2024-09-30 22:13:44 -0300

    fix: axios credentials

--- src/api/numericSolveApi.ts
+++ src/api/numericSolveApi.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 const numericSolveApi = axios.create({
   baseURL: "https://api-calculonumerico.onrender.com/",
+  withCredentials: true,
 });
 export default numericSolveApi;
